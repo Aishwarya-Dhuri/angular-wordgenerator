f08610a75bf004973a680f926c3b49538750e67b
Aishwarya-Dhuri <aishwaryadhuri456@gmail.com>
2021-04-05 15:36:53 +0530

    word generator live

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import arrayWords from "../utils/words";
+import countryWords from "../utils/countries";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'word-generator';
+
+  words = '';
+  limit = 10;
+  random = 10;
+  countries = '';
+
+  handleSlideChange(newLimit : number){
+    this.limit = newLimit;
+    this.random = Math.ceil(Math.random() * 249);
+  }
+
+  generate(){
+    this.words = arrayWords.slice(0,this.limit).join(' ');
+  }
+
+  generateCountry(){
+  this.countries =  countryWords.slice(0,this.random).join(',');
+  }
+ 
+
 }
---
+++ src/utils/countries.ts
@@ -0,0 +1,32 @@
+export default [
+   'Australia',
+   'Mexico',
+   'India',
+   'Moldova',
+   'Nepal',
+   'New Zealand',
+   'Niger',
+   'Canada',
+   'Chile',
+   'Russia',
+   'Egypt',
+   'San Marino',
+    'Serbia',
+    'South Africa',
+    'Switzerland',
+    'Syria',
+
+    'Greece',
+
+    'Thailand',
+    'Indonesia',
+    'Iran',
+    'Iraq',
+
+    'Japan',
+    'Kenya',
+    'Luxembourg',
+    'Zimbabwe',
+    'Turkey',
+
+];
